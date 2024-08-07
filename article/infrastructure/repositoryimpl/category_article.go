a458da0bf85a13d9cff01c728639e6650b06824a
VictorZhou123 <victorzhoux@163.com>
2024-08-08 01:26:36 +0800

    feat: domain service of add category_article relation

---
+++ article/infrastructure/repositoryimpl/category_article.go
@@ -0,0 +1,30 @@
+package repositoryimpl
+
+import (
+	"victorzhou123/vicblog/article/domain/category/repository"
+	cmprimitive "victorzhou123/vicblog/common/domain/primitive"
+	"victorzhou123/vicblog/common/infrastructure/mysql"
+)
+
+func NewCategoryArticleRepo(db mysql.Impl) repository.CategoryArticle {
+	tableNameCategoryArticle = db.TableName()
+
+	if err := mysql.AutoMigrate(&CategoryArticleDO{}); err != nil {
+		return nil
+	}
+
+	return &categoryArticleImpl{db}
+}
+
+type categoryArticleImpl struct {
+	mysql.Impl
+}
+
+func (impl *categoryArticleImpl) BindCategoryAndArticle(articleId, cateId cmprimitive.Id) error {
+	do := CategoryArticleDO{
+		CategoryId: cateId.IdNum(),
+		ArticleId:  articleId.IdNum(),
+	}
+
+	return impl.Impl.Add(&do)
+}
