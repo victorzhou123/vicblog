4ee2846b4bd961b5cc86053facf5028fb3183ac9
victorzhou123 <victorzhoux@163.com>
2024-08-22 11:46:28 +0800

    refactor: refactor server code

---
+++ server/wire.go
@@ -0,0 +1,129 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	smq "github.com/victorzhou123/simplemq/mq"
+	articleappevent "github.com/victorzhou123/vicblog/article/app/event"
+	articleappsvc "github.com/victorzhou123/vicblog/article/app/service"
+	articlectl "github.com/victorzhou123/vicblog/article/controller"
+	articlesvc "github.com/victorzhou123/vicblog/article/domain/article/service"
+	categorysvc "github.com/victorzhou123/vicblog/article/domain/category/service"
+	picturesvc "github.com/victorzhou123/vicblog/article/domain/picture/service"
+	tagsvc "github.com/victorzhou123/vicblog/article/domain/tag/service"
+	articlerepoimpl "github.com/victorzhou123/vicblog/article/infrastructure/repositoryimpl"
+	blogappsvc "github.com/victorzhou123/vicblog/blog/app/service"
+	blogctl "github.com/victorzhou123/vicblog/blog/controller"
+	blogsvc "github.com/victorzhou123/vicblog/blog/domain/service"
+	blogrepoimpl "github.com/victorzhou123/vicblog/blog/infrastructure/repositoryimpl"
+	cmapp "github.com/victorzhou123/vicblog/common/app"
+	cminfraauthimpl "github.com/victorzhou123/vicblog/common/infrastructure/authimpl"
+	"github.com/victorzhou123/vicblog/common/infrastructure/eventimpl"
+	"github.com/victorzhou123/vicblog/common/infrastructure/md2htmlimpl"
+	"github.com/victorzhou123/vicblog/common/infrastructure/mqimpl"
+	cminframysql "github.com/victorzhou123/vicblog/common/infrastructure/mysql"
+	"github.com/victorzhou123/vicblog/common/infrastructure/oss"
+	cmutil "github.com/victorzhou123/vicblog/common/util"
+	mconfig "github.com/victorzhou123/vicblog/config"
+	_ "github.com/victorzhou123/vicblog/docs"
+	statsappevent "github.com/victorzhou123/vicblog/statistics/app/event"
+	statsappsvc "github.com/victorzhou123/vicblog/statistics/app/service"
+	statsctl "github.com/victorzhou123/vicblog/statistics/controller"
+	statssvc "github.com/victorzhou123/vicblog/statistics/domain/service"
+	statsimpl "github.com/victorzhou123/vicblog/statistics/infrastructure/repositoryimpl"
+	userapp "github.com/victorzhou123/vicblog/user/app"
+	userctl "github.com/victorzhou123/vicblog/user/controller"
+	userrepoimpl "github.com/victorzhou123/vicblog/user/infrastructure/repositoryimpl"
+)
+
+func setRouters(engine *gin.Engine, cfg *mconfig.Config) {
+
+	// infrastructure: following are the instance of infrastructure components
+	mq := mqimpl.MQ()
+	timeCreator := cmutil.NewTimerCreator()
+	mysqlImpl := cminframysql.DAO()
+	transactionImpl := cminframysql.NewTransaction()
+	m2h := md2htmlimpl.NewMd2Html()
+	publisher := eventimpl.NewPublisher(mq)
+
+	// repo: following are the dependencies of service
+	ossRepo := articlerepoimpl.NewPictureImpl(oss.Client())
+	auth := cminfraauthimpl.NewSignJwt(&timeCreator, &cfg.Common.Infra.Auth)
+	userRepo := userrepoimpl.NewUserRepo(mysqlImpl)
+	articleRepo := articlerepoimpl.NewArticleRepo(mysqlImpl, transactionImpl)
+	categoryRepo := articlerepoimpl.NewCategoryRepo(mysqlImpl)
+	categoryArticleRepo := articlerepoimpl.NewCategoryArticleRepo(mysqlImpl, transactionImpl)
+	tagRepo := articlerepoimpl.NewTagRepo(mysqlImpl)
+	tagArticleRepo := articlerepoimpl.NewTagArticleRepo(mysqlImpl, transactionImpl)
+	blogRepo := blogrepoimpl.NewBlogInfoImpl(&cfg.Blog.BlogInfo)
+	statsRepo := statsimpl.NewArticleVisitsRepo(mysqlImpl, &timeCreator)
+
+	// domain: following are domain services
+	tagService := tagsvc.NewTagService(tagRepo, tagArticleRepo)
+	articleService := articlesvc.NewArticleService(articleRepo, m2h, &timeCreator)
+	categoryService := categorysvc.NewCategoryService(categoryRepo, categoryArticleRepo)
+	pictureService := picturesvc.NewFileService(ossRepo)
+	blogService := blogsvc.NewBlogService(blogRepo)
+	articleVisitsService := statssvc.NewArticleVisitsService(statsRepo)
+
+	// app: following are app services
+	authMiddleware := cmapp.NewAuthMiddleware(auth)
+	loginService := userapp.NewLoginService(userRepo, auth)
+	articleAppService := articleappsvc.NewArticleAppService(transactionImpl, articleService, categoryService, tagService, publisher)
+	cateAppService := articleappsvc.NewCategoryAppService(categoryService)
+	tagAppService := articleappsvc.NewTagAppService(tagService)
+	blogAppService := blogappsvc.NewBlogAppService(blogService)
+	dashboardAppService := statsappsvc.NewDashboardAppService(articleService, tagService, categoryService, articleVisitsService)
+	articleVisitsAppService := statsappsvc.NewArticleVisitsAppService(articleVisitsService)
+
+	// subscriber
+	articleSubscriber := articleappevent.NewArticleSubscriber(articleService)
+	statsSubscriber := statsappevent.NewArticleVisitsSubscriber(articleVisitsService)
+
+	// controller: add routers
+	v1 := engine.Group(BasePath)
+	{
+		addRouterForSwaggo(v1)
+
+		userctl.AddRouterForLoginController(
+			v1, loginService,
+		)
+
+		articlectl.AddRouterForArticleController(
+			v1, authMiddleware, articleService, articleAppService,
+		)
+
+		articlectl.AddRouterForCategoryController(
+			v1, authMiddleware, cateAppService,
+		)
+
+		articlectl.AddRouterForTagController(
+			v1, authMiddleware, tagAppService,
+		)
+
+		articlectl.AddRouterForFileController(
+			v1, authMiddleware, pictureService,
+		)
+
+		blogctl.AddRouterForBlogController(
+			v1, blogAppService,
+		)
+
+		statsctl.AddRouterForStatisticsController(
+			v1, dashboardAppService, articleVisitsAppService,
+		)
+	}
+
+	// distributer to distribute message
+	distributer := newDistributer(articleSubscriber, statsSubscriber)
+
+	// watch
+	mqWatcher := smq.NewWatcher(mq, distributer)
+	go mqWatcher.Watch()
+}
+
+func addRouterForSwaggo(rg *gin.RouterGroup) {
+	rg.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+}
