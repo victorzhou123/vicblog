951c69c77f6a881984de6da2d89991a198f34d2d
victorzhou123 <victorzhoux@163.com>
2025-03-18 17:46:37 +0800

    test pr code review robot

--- main.go
+++ main.go
@@ -11,19 +11,9 @@ import (
 	"github.com/victorzhou123/vicblog/server"
 )
 
-// @title            vicBlog server API
-// @version        1.0
-//
-// @contact.name    VictorZhou
-// @contact.email    victorzhoux@163.com
-//
-// @host        localhost:8080
-// @BasePath    /api
 func main() {
 	exitSig := make(chan struct{})
-	defer func() {
-		exitSig <- struct{}{}
-	}()
+	defer func() { exitSig <- struct{}{} }()
 
 	// config
 	cfg := new(config.Config)
@@ -32,26 +22,20 @@ func main() {
 		return
 	}
 
-	// log
 	log.Init(&cfg.Common.Log, exitSig)
 
-	// mysql
 	if err := mysql.Init(&cfg.Common.Infra.Mysql); err != nil {
-		log.Warnf("mysql init failed, error: %s", err.Error())
-		return
+		log.Fatalf("MySQL初始化失败: %v", err)
 	}
 
-	// object storage
 	if err := oss.Init(&cfg.Common.Infra.Oss); err != nil {
-		log.Warnf("object storage init failed, error: %s", err.Error())
-		return
+		log.Fatalf("OSS初始化失败: %v", err)
 	}
 
-	// mq
 	mq := kafka.NewKafka(&cfg.Common.Infra.Kafka)
 	defer mq.Close()
 
 	if err := server.StartWebServer(cfg, mq); err != nil {
-		log.Fatalf("start web server error: %s", err.Error())
+		log.Fatalf("服务器启动失败: %v", err)
 	}
 }
